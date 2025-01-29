9865aa3e1512e340d51cb289b1e4957f1fef8e5f
Maxime <maximeye1234@gmail.com>
2025-01-29 15:20:54 +0100

    add project

---
+++ src/main/java/fr/efrei/pokemon_tcg/security/AuthenticationService.java
@@ -0,0 +1,21 @@
+package fr.efrei.pokemon_tcg.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+public class AuthenticationService {
+
+	private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
+	private static final String AUTH_TOKEN = "741794db15ed9780a43d4fb42bfde435af6cc5e8c99a6b07d7be696dad7ffbec";
+
+	public static Authentication getAuthentication(HttpServletRequest request) {
+		String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+		if (apiKey == null || !apiKey.equals(AUTH_TOKEN)) {
+			throw new BadCredentialsException("Invalid API Key");
+		}
+
+		return new ApiKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
+	}
+}
